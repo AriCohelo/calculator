f2f404bc7416d77b201071ffa85b98ddc5bab6a7
AriCohelo <ariel.cohelo@gmail.com>
2023-07-18 17:51:00 +0200

    agregados todos los botones numericos y de operadores y cada uno cumple su funcion. Falta solucionar el conflicto entre la funcion clear y equal.

--- code.js
+++ code.js
@@ -3,44 +3,302 @@ display.textContent = ''
 
 let dot = document.getElementById('dot')
 dot.addEventListener('click', function () {
-
+    if (display.textContent === '') {
+        return;
+    }
+    if (result !== 0) {
+        clearFunc()
+        return
+    }
     display.textContent += '.';
 })
+let result = 0;
+
+let zero = document.getElementById('zero');
+zero.addEventListener('click', function () {
+    if (result !== 0) {
+        clearFunc()
+    }
+    display.textContent += 0
+})
+
 let one = document.getElementById('one');
 one.addEventListener('click', function () {
-
+    if (result !== 0) {
+        clearFunc()
+    }
     display.textContent += 1
 })
 
+let two = document.getElementById('two');
+two.addEventListener('click', function () {
+    if (result !== 0) {
+        clearFunc()
+    }
+    display.textContent += 2
+})
+
+let three = document.getElementById('three');
+three.addEventListener('click', function () {
+    if (result !== 0) {
+        clearFunc()
+    }
+    display.textContent += 3
+})
+
+let four = document.getElementById('four');
+four.addEventListener('click', function () {
+    if (result !== 0) {
+        clearFunc()
+    }
+    display.textContent += 4
+})
+
+let five = document.getElementById('five');
+five.addEventListener('click', function () {
+    if (result !== 0) {
+        clearFunc()
+    }
+    display.textContent += 5
+})
+
+let six = document.getElementById('six');
+six.addEventListener('click', function () {
+    if (result !== 0) {
+        clearFunc()
+    }
+    display.textContent += 6
+})
+
+let seven = document.getElementById('seven');
+seven.addEventListener('click', function () {
+    if (result !== 0) {
+        clearFunc()
+    }
+    display.textContent += 7
+})
+
+let eight = document.getElementById('eight');
+eight.addEventListener('click', function () {
+    if (result !== 0) {
+        clearFunc()
+    }
+    display.textContent += 8
+})
+
+let nine = document.getElementById('nine');
+nine.addEventListener('click', function () {
+    if (result !== 0) {
+        clearFunc()
+    }
+    display.textContent += 9
+})
+
+// Operator Buttons
 
 let add = document.getElementById('add');
 add.addEventListener('click', function () {
-    let splited = display.textContent.split('+');
-    if (splited.length === 2) {
+    let splitedAdd = display.textContent.split('+');
+    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+        return;
+    }
+    if (splitedAdd.length === 2) {
+
         equalFunc()
     }
-
 })
 add.addEventListener('click', function () {
-
+    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+        return;
+    }
     display.textContent += '+';
 })
+
+let subs = document.getElementById('subs');
+subs.addEventListener('click', function () {
+    let splitedAdd = display.textContent.split('-');
+    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+        return;
+    }
+    if (splitedAdd.length === 2) {
+
+        equalFunc()
+    }
+})
+subs.addEventListener('click', function () {
+    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+        return;
+    }
+    display.textContent += '-';
+})
+
+
+
+let division = document.getElementById('division');
+division.addEventListener('click', function () {
+    let splitedDiv = display.textContent.split('/');
+    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+        return;
+    }
+    if (splitedDiv.length === 2) {
+
+        equalFunc()
+    }
+})
+division.addEventListener('click', function () {
+    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+        return;
+    }
+    display.textContent += '/';
+})
+
+let multi = document.getElementById('multi');
+multi.addEventListener('click', function () {
+    let splitedMulti = display.textContent.split('*');
+    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+        return;
+    }
+    if (splitedMulti.length === 2) {
+
+        equalFunc()
+    }
+})
+multi.addEventListener('click', function () {
+    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+        return;
+    }
+    display.textContent += '*';
+})
+
+
+
+
+
+
+
+
+
+
 
 let equal = document.getElementById('equal');
 equal.addEventListener('click', equalFunc)
 
 function equalFunc() {
-    let splited = display.textContent.split('+');
-    console.log(splited);
+    let parsedAdd = [];
+    let parsedSubs = [];
+    let parsedDiv = [];
+    let parsedMulti = [];
+    let splitedAdd = [];
+    let splitedSubs = [];
+    let splitedDiv = [];
+    let splitedMulti = [];
     if (display.textContent.includes('+')) {
+        splitedAdd = display.textContent.split('+');
+    }
+    if (display.textContent.includes('-')) {
+        splitedSubs = display.textContent.split('-');
+    }
+    if (display.textContent.includes('/')) {
+        splitedDiv = display.textContent.split('/');
+    }
+    if (display.textContent.includes('*')) {
+        splitedMulti = display.textContent.split('*');
+    }
+    //Split Add
 
-        suma(splited)
+    if (Number.isInteger(Number(splitedAdd[0]))) {
+        parsedAdd.push(parseInt(splitedAdd[0]))
+    } else {
+        parsedAdd.push(parseFloat(splitedAdd[0]))
+    }
+    if (Number.isInteger(Number(splitedAdd[1]))) {
+        parsedAdd.push(parseInt(splitedAdd[1]))
+    } else {
+        parsedAdd.push(parseFloat(splitedAdd[1]))
     }
 
+    //split Subs
+
+    if (Number.isInteger(Number(splitedSubs[0]))) {
+        parsedSubs.push(parseInt(splitedSubs[0]))
+    } else {
+        parsedSubs.push(parseFloat(splitedSubs[0]))
+    }
+
+    if (Number.isInteger(Number(splitedSubs[1]))) {
+        parsedSubs.push(parseInt(splitedSubs[1]))
+    } else {
+        parsedSubs.push(parseFloat(splitedSubs[1]))
+    }
+
+
+    // Split Div
+
+
+    if (Number.isInteger(Number(splitedDiv[0]))) {
+        parsedDiv.push(parseInt(splitedDiv[0]))
+    } else {
+        parsedDiv.push(parseFloat(splitedDiv[0]))
+    }
+    if (Number.isInteger(Number(splitedDiv[1]))) {
+        parsedDiv.push(parseInt(splitedDiv[1]))
+    } else {
+        parsedDiv.push(parseFloat(splitedDiv[1]))
+    }
+
+    // // Splited Multi
+
+    if (Number.isInteger(Number(splitedMulti[0]))) {
+        parsedMulti.push(parseInt(splitedMulti[0]))
+    } else {
+        parsedMulti.push(parseFloat(splitedMulti[0]))
+    }
+    if (Number.isInteger(Number(splitedMulti[1]))) {
+        parsedMulti.push(parseInt(splitedMulti[1]))
+    } else {
+        parsedMulti.push(parseFloat(splitedMulti[1]))
+    }
+
+    //Call Func
+
+    if (display.textContent.includes('+')) {
+        addFunc(parsedAdd)
+    }
+    if (display.textContent.includes('-')) {
+        subsFunc(parsedSubs)
+    }
+    if (display.textContent.includes('/')) {
+        divFunc(parsedDiv)
+    }
+    if (display.textContent.includes('*')) {
+        multiFunc(parsedMulti)
+    }
 }
 
+let clear = document.getElementById('clear');
+clear.addEventListener('click', clearFunc)
 
-function suma(splited) {
-    result = parseInt(splited[0]) + parseInt(splited[1]);
+function clearFunc() {
+    display.textContent = '';
+    result = 0;
+}
+
+function addFunc(parsedAdd) {
+    result = parsedAdd[0] + parsedAdd[1];
+    display.textContent = result;
+};
+
+function subsFunc(parsedNumbers) {
+    result = parsedNumbers[0] - parsedNumbers[1];
+    display.textContent = result;
+};
+
+function divFunc(parsedDiv) {
+    result = parsedDiv[0] / parsedDiv[1];
+    display.textContent = result;
+};
+
+function multiFunc(parsedMulti) {
+    result = parsedMulti[0] * parsedMulti[1];
     display.textContent = result;
 };
