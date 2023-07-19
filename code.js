b46ffb103c7122fdc2260e83955f6cbeb86d4396
AriCohelo <ariel.cohelo@gmail.com>
2023-07-19 15:31:01 +0200

    terminada la logica de la calculardora los botones y el display para que no haya overflow. Tambien agregada la logica para que no haya demasiados decimales. Falta agregar soporte para teclado

--- code.js
+++ code.js
@@ -1,188 +1,147 @@
+let result = 0;
 let display = document.getElementById('display');
 display.textContent = ''
-
 let dot = document.getElementById('dot')
 dot.addEventListener('click', function () {
     if (display.textContent === '') {
         return;
     }
-    if (result !== 0) {
-        clearFunc()
-        return
-    }
+
     display.textContent += '.';
 })
-let result = 0;
 
 let zero = document.getElementById('zero');
 zero.addEventListener('click', function () {
-    if (result !== 0) {
-        clearFunc()
-    }
+
     display.textContent += 0
 })
 
 let one = document.getElementById('one');
 one.addEventListener('click', function () {
-    if (result !== 0) {
-        clearFunc()
-    }
+
     display.textContent += 1
 })
 
 let two = document.getElementById('two');
 two.addEventListener('click', function () {
-    if (result !== 0) {
-        clearFunc()
-    }
+
     display.textContent += 2
 })
 
 let three = document.getElementById('three');
 three.addEventListener('click', function () {
-    if (result !== 0) {
-        clearFunc()
-    }
+
     display.textContent += 3
 })
 
 let four = document.getElementById('four');
 four.addEventListener('click', function () {
-    if (result !== 0) {
-        clearFunc()
-    }
+
     display.textContent += 4
 })
 
 let five = document.getElementById('five');
 five.addEventListener('click', function () {
-    if (result !== 0) {
-        clearFunc()
-    }
+
     display.textContent += 5
 })
 
 let six = document.getElementById('six');
 six.addEventListener('click', function () {
-    if (result !== 0) {
-        clearFunc()
-    }
+
     display.textContent += 6
 })
 
 let seven = document.getElementById('seven');
 seven.addEventListener('click', function () {
-    if (result !== 0) {
-        clearFunc()
-    }
+
     display.textContent += 7
 })
 
 let eight = document.getElementById('eight');
 eight.addEventListener('click', function () {
-    if (result !== 0) {
-        clearFunc()
-    }
+
     display.textContent += 8
 })
 
 let nine = document.getElementById('nine');
 nine.addEventListener('click', function () {
-    if (result !== 0) {
-        clearFunc()
-    }
+
     display.textContent += 9
 })
 
 // Operator Buttons
 
 let add = document.getElementById('add');
-add.addEventListener('click', function () {
+add.addEventListener('click', appendPlus);
+function appendPlus() {
     let splitedAdd = display.textContent.split('+');
-    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' ||
+        display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
         return;
     }
     if (splitedAdd.length === 2) {
-
         equalFunc()
     }
-})
-add.addEventListener('click', function () {
-    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+    if (display.textContent === '') {
         return;
     }
     display.textContent += '+';
-})
+}
 
 let subs = document.getElementById('subs');
-subs.addEventListener('click', function () {
+subs.addEventListener('click', appendMinus);
+function appendMinus() {
     let splitedAdd = display.textContent.split('-');
-    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' ||
+        display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
         return;
     }
     if (splitedAdd.length === 2) {
-
         equalFunc()
     }
-})
-subs.addEventListener('click', function () {
-    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+    if (display.textContent === '') {
         return;
     }
     display.textContent += '-';
-})
-
-
+}
 
 let division = document.getElementById('division');
-division.addEventListener('click', function () {
+division.addEventListener('click', appendDiv);
+function appendDiv() {
     let splitedDiv = display.textContent.split('/');
-    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' ||
+        display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
         return;
     }
     if (splitedDiv.length === 2) {
-
         equalFunc()
     }
-})
-division.addEventListener('click', function () {
-    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+    if (display.textContent === '') {
         return;
     }
     display.textContent += '/';
-})
+}
 
 let multi = document.getElementById('multi');
-multi.addEventListener('click', function () {
+multi.addEventListener('click', appendMulti)
+function appendMulti() {
     let splitedMulti = display.textContent.split('*');
-    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' ||
+        display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
         return;
     }
     if (splitedMulti.length === 2) {
-
         equalFunc()
     }
-})
-multi.addEventListener('click', function () {
-    if (display.textContent.slice(-1) === '+' || display.textContent.slice(-1) === '-' || display.textContent.slice(-1) === '/' || display.textContent.slice(-1) === '*') {
+    if (display.textContent === '') {
         return;
     }
     display.textContent += '*';
-})
-
-
-
-
-
-
-
-
-
-
+}
 
 let equal = document.getElementById('equal');
 equal.addEventListener('click', equalFunc)
-
 function equalFunc() {
     let parsedAdd = [];
     let parsedSubs = [];
@@ -204,6 +163,7 @@ function equalFunc() {
     if (display.textContent.includes('*')) {
         splitedMulti = display.textContent.split('*');
     }
+
     //Split Add
 
     if (Number.isInteger(Number(splitedAdd[0]))) {
@@ -216,24 +176,19 @@ function equalFunc() {
     } else {
         parsedAdd.push(parseFloat(splitedAdd[1]))
     }
-
     //split Subs
-
     if (Number.isInteger(Number(splitedSubs[0]))) {
         parsedSubs.push(parseInt(splitedSubs[0]))
     } else {
         parsedSubs.push(parseFloat(splitedSubs[0]))
     }
-
     if (Number.isInteger(Number(splitedSubs[1]))) {
         parsedSubs.push(parseInt(splitedSubs[1]))
     } else {
         parsedSubs.push(parseFloat(splitedSubs[1]))
     }
 
-
     // Split Div
-
 
     if (Number.isInteger(Number(splitedDiv[0]))) {
         parsedDiv.push(parseInt(splitedDiv[0]))
@@ -246,7 +201,7 @@ function equalFunc() {
         parsedDiv.push(parseFloat(splitedDiv[1]))
     }
 
-    // // Splited Multi
+    // Splited Multi
 
     if (Number.isInteger(Number(splitedMulti[0]))) {
         parsedMulti.push(parseInt(splitedMulti[0]))
@@ -275,6 +230,13 @@ function equalFunc() {
     }
 }
 
+let backspace = document.getElementById('backspace');
+backspace.addEventListener('click', backspaceFunc);
+
+function backspaceFunc() {
+    display.textContent = display.textContent.slice(0, -1);
+}
+
 let clear = document.getElementById('clear');
 clear.addEventListener('click', clearFunc)
 
@@ -282,7 +244,6 @@ function clearFunc() {
     display.textContent = '';
     result = 0;
 }
-
 function addFunc(parsedAdd) {
     result = parsedAdd[0] + parsedAdd[1];
     display.textContent = result;
@@ -295,6 +256,9 @@ function subsFunc(parsedNumbers) {
 
 function divFunc(parsedDiv) {
     result = parsedDiv[0] / parsedDiv[1];
+    if (!Number.isInteger(result)) {
+        result = result.toFixed(6);
+    }
     display.textContent = result;
 };
 
@@ -302,3 +266,5 @@ function multiFunc(parsedMulti) {
     result = parsedMulti[0] * parsedMulti[1];
     display.textContent = result;
 };
+
+// Solucionar spliteAdd muchas veces
