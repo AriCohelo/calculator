aff281bf4bbf1f62173ca927b3c1bb6f97c06cb2
AriCohelo <ariel.cohelo@gmail.com>
2023-07-25 16:48:32 +0200

    terminado. He agregado el segundo display para que este dentro del display principal y corregido la cantidd de decimales

--- code.js
+++ code.js
@@ -1,7 +1,8 @@
 let operand1 = undefined;
 let operand2 = undefined;
+let operators = ['+', '-', '/', '*'];
+let operatorsChoice = '';
 let result = undefined;
-
 let display = document.getElementById('display');
 let secondDisplay = document.getElementById('secondDisplay');
 
@@ -18,7 +19,7 @@ dot.addEventListener('click', function () {
     display.textContent += '.';
 })
 
-const numberButtons = document.querySelectorAll('.numberButton');
+let numberButtons = document.querySelectorAll('.numberButton');
 numberButtons.forEach(button => {
     button.addEventListener('click', function () {
         button.style.backgroundColor = '#E36E00';
@@ -43,90 +44,81 @@ operatorButtons.forEach(button => {
             button.style.backgroundColor = '';
         }, 200);
         if (button.textContent != '=') {
-
-            if (secondDisplay.textContent.slice(-1) === '+' && display.textContent != '') {
-                if (Number.isInteger(Number(secondDisplay.textContent.slice(0, -1)))) {
-
-                    operand1 = parseInt(secondDisplay.textContent.slice(0, -1));
-                } else {
-                    operand1 = parseFloat(secondDisplay.textContent.slice(0, -1));
+            for (let i = 0; i < operators.length; i++) {
+                if (secondDisplay.textContent.slice(-1) === operators[i] && display.textContent != '') {
+                    operatorsChoice = button.textContent;
+                    if (Number.isInteger(Number(secondDisplay.textContent.slice(0, -1)))) {
+                        operand1 = parseInt(secondDisplay.textContent.slice(0, -1));
+                    } else {
+                        operand1 = parseFloat(secondDisplay.textContent.slice(0, -1));
+                    }
+                    if (Number.isInteger(Number(display.textContent))) {
+                        operand2 = parseInt(display.textContent);
+                    } else {
+                        operand2 = parseFloat(display.textContent);
+                    }
+                    chainedOperation()
+                    return;
                 }
-                if (Number.isInteger(Number(display.textContent))) {
-
-                    operand2 = parseInt(display.textContent);
-                } else {
-                    operand2 = parseFloat(display.textContent);
-                }
-                console.log('operand2 ' + operand2)
-                chainedOperation()
-                return;
             }
             if (Number.isInteger(Number(display.textContent))) {
-
                 operand1 = parseInt(display.textContent);
             } else {
                 operand1 = parseFloat(display.textContent);
             }
             display.textContent = '';
             secondDisplay.textContent = operand1 + button.textContent;
-
-            console.log('operand1 ' + operand1);
         }
     })
 })
 
 function chainedOperation() {
-    if (secondDisplay.textContent.includes('+')) {
+    if (secondDisplay.textContent.slice(-1) === '+') {
         addFunc(operand1, operand2);
     }
-    if (secondDisplay.textContent.includes('-')) {
+    if (secondDisplay.textContent.slice(-1) === '-') {
         subsFunc(operand1, operand2);
     }
-    if (secondDisplay.textContent.includes('/')) {
+    if (secondDisplay.textContent.slice(-1) === '/') {
         divFunc(operand1, operand2);
     }
-    if (secondDisplay.textContent.includes('*')) {
+    if (secondDisplay.textContent.slice(-1) === '*') {
         multiFunc(operand1, operand2);
     }
-    secondDisplay.textContent = result + '+';
+    secondDisplay.textContent = result + operatorsChoice;
     display.textContent = '';
-
     operand2 = undefined;
     operand1 = undefined;
 }
+
 let equal = document.getElementById('equal');
 equal.addEventListener('click', equalFunc)
 function equalFunc() {
-
     if (Number.isInteger(Number(secondDisplay.textContent.slice(0, -1)))) {
-
         operand1 = parseInt(secondDisplay.textContent.slice(0, -1));
     } else {
         operand1 = parseFloat(secondDisplay.textContent.slice(0, -1));
     }
     if (Number.isInteger(Number(display.textContent))) {
-
         operand2 = parseInt(display.textContent);
     } else {
         operand2 = parseFloat(display.textContent);
     }
     display.textContent = '';
-    secondDisplay.textContent += operand2 + '=';
-    console.log('operand 2 ' + operand2);
-    console.log('operand 1 ' + operand1);
 
-    if (secondDisplay.textContent.includes('+')) {
+    if (secondDisplay.textContent.slice(-1) === '+') {
         addFunc(operand1, operand2);
     }
-    if (secondDisplay.textContent.includes('-')) {
+    if (secondDisplay.textContent.slice(-1) === '-') {
         subsFunc(operand1, operand2);
     }
-    if (secondDisplay.textContent.includes('/')) {
+    if (secondDisplay.textContent.slice(-1) === '/') {
         divFunc(operand1, operand2);
     }
-    if (secondDisplay.textContent.includes('*')) {
+    if (secondDisplay.textContent.slice(-1) === '*') {
         multiFunc(operand1, operand2);
     }
+    secondDisplay.textContent += operand2 + '=';
     operand1 = undefined;
     operand2 = undefined;
 }
@@ -147,38 +139,32 @@ function clearFunc() {
 
 function addFunc(op1, op2) {
     result = op1 + op2;
-    // if (!Number.isInteger(result)) {
-    //     result = result.toFixed(6);
-    // }
-    console.log('result ' + result);
+    if (!Number.isInteger(result) && result.toString().length > 8) {
+        result = result.toFixed(7);
+    }
     display.textContent = result;
 };
 
 function subsFunc(op1, op2) {
     result = op1 - op2;
-    if (!Number.isInteger(result)) {
-        result = result.toFixed(6);
+    if (!Number.isInteger(result) && result.toString().length > 8) {
+        result = result.toFixed(7);
     }
-    console.log('result ' + result);
     display.textContent = result;
 };
 
 function divFunc(op1, op2) {
     result = op1 / op2;
-    if (!Number.isInteger(result)) {
-        result = result.toFixed(6);
+    if (!Number.isInteger(result) && result.toString().length > 8) {
+        result = result.toFixed(7);
     }
-    console.log('result ' + result);
     display.textContent = result;
 };
 
 function multiFunc(op1, op2) {
     result = op1 * op2;
-    if (!Number.isInteger(result)) {
-        result = result.toFixed(6);
+    if (!Number.isInteger(result) && result.toString().length > 8) {
+        result = result.toFixed(7);
     }
-    console.log('result ' + result);
     display.textContent = result;
 };
-
-
